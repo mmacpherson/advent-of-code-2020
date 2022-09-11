bd9c0562e39f0e4efdb8217c8c73ce867bb0dc00
Mike Macpherson <mmacpherson@users.noreply.github.com>
2022-09-11 10:01:32 -0700

    day 7 part 2

--- day7.jl
+++ day7.jl
@@ -1,5 +1,9 @@
 using Graphs
 
+"""
+Didn't need to use the `Graphs` stuff for this one, in retrospect.
+"""
+
 function nobag(s)
     no = [" bags.", " bags", " bag.", " bag"]
     for e in no
@@ -23,6 +27,10 @@ function parse_input(fname)
     end
     out
 end
+
+ex07 = parse_input("data/ex07")
+ex07_1 = parse_input("data/ex07-1")
+in07 = parse_input("data/in07")
 
 function day7_1(input)
 
@@ -54,12 +62,35 @@ function day7_1(input)
         can_hold_gold = union(can_hold_gold, parents)
         process_queue = union(process_queue, parents)
     end
-
     length(can_hold_gold)
 end
 
-ex07 = parse_input("data/ex07")
-in07 = parse_input("data/in07")
+
+function day7_2(input)
+
+    num_bags = 0
+    process_queue = [("shiny gold", 1)]
+    while !isempty(process_queue)
+        # println(">>>")
+        # println("pq ", process_queue)
+        (node, node_count) = pop!(process_queue)
+        # println("node: ", node)
+        children = input[node]
+        # println("children: ", children)
+        if isnothing(children)
+            continue
+        end
+        for child in children
+            push!(process_queue, (child["bag"], node_count * child["count"]))
+            num_bags += node_count * child["count"]
+        end
+        # println("current count: ", num_bags)
+        # println("<<<\n")
+    end
+    num_bags
+end
 
 
 [day7_1(ex07), day7_1(in07)]
+
+[day7_2(ex07), day7_2(ex07_1), day7_2(in07)]
