fbaff07b935b48f62440077089155ff666c2f6c3
Mike Macpherson <mmacpherson@users.noreply.github.com>
2022-10-01 10:53:05 -0700

    day 9 part 2

--- day9.jl
+++ day9.jl
@@ -33,8 +33,42 @@ function first_incompatible(xs, window)
     nothing
 end
 
+function locate_weakness(xs, window)
+
+    invalid_number = first_incompatible(xs, window)
+
+    acc = cumsum(xs)
+
+    for ix = 1:(length(xs)-1)
+        for jx = (ix+1):length(xs)
+            if ix == 1
+                ss = acc[jx]
+            else
+                ss = acc[jx] - acc[ix-1]
+            end
+            if ss == invalid_number
+                span = xs[ix:jx]
+                return minimum(span) + maximum(span)
+            elseif ss > invalid_number
+                # Numbers are all positive, so once we've gone past
+                # invalid_number no longer possible to equal invalid_number.
+                continue
+            end
+        end
+    end
+
+
+end
+
 function day9_1(input)
     first_incompatible(input, 25)
 end
 
+function day9_2(input)
+    locate_weakness(input, 25)
+end
+
 [first_incompatible(ex09, 5), day9_1(in09)]
+
+
+[locate_weakness(ex09, 5), day9_2(in09)]
